1fc07de6176340cbff87789821b6b57a54fe5ec3
tinphan9x <86870659+tinphan9x@users.noreply.github.com>
2021-10-13 16:42:14 +0700

    Update TestRunner.py

--- TestSuites/TestRunner.py
+++ TestSuites/TestRunner.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     #os.system("C:/Users/USER/scoop/apps/allure/2.14.0/bin/allure generate --clean --output " + './Reports')
     #Run allure report of this file, export report to PJ/Reports
-    pytest.main(['-s', '-q','--alluredir','./allure-report','./TestCases/Test_Login.py'])
-    #pytest.main(['-s', '-q','./TestCases/Test_Login.py'])
+    #pytest.main(['-s', '-q','--alluredir','./allure-report','./TestCases/Test_Login.py'])
+    pytest.main(['-s', '-q','./TestCases/Test_Login.py'])
     #Open allue report via browser
     #os.system("C:/Users/USER/scoop/apps/allure/2.14.0/bin/allure serve " + './Reports')
